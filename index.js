4b1567541b174415c7634d660aa52c28e56308bd
Marcos <m.rodrig.felix@gmail.com>
2021-02-21 19:35:59 -0800

    commit tree ef1327447a6f32dbde238d4e6f22aba786e4df27

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+
+function findMatching(driversArray, string){
+  const matchingDrivers = driversArray.filter( driver => {
+    return driver.toLowerCase() === string.toLowerCase()
+  })
+
+  return matchingDrivers.length > 0 ? matchingDrivers : [];
+};
+
+
+
+function fuzzyMatch(driversArray, string){
+  const matchingDrivers = driversArray.filter( driver => {
+    return driver.toLowerCase()[0] === string.toLowerCase()[0]
+  })
+
+  return matchingDrivers;
+};
+
+
+
+function matchName(driversArray, string){
+  const matchingDrivers = driversArray.filter( driver => {
+    return driver.name.toLowerCase() === string.toLowerCase()
+  })
+
+  return matchingDrivers;
+};
